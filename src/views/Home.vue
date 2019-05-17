7e778a8864853ed9bc1b86c0b85e71cb45b3a6bb
munseong.kim <iris4865@gmail.com>
2019-05-17 17:18:36 +0900

    Test axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">{{ todolist }}</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  name: "home",
+  data() {
+    return {
+      todolist: []
+    };
   },
+
+  methods: {
+    getTodoList() {
+      const path = "http://localhost:5000/todolist";
+
+      axios
+        .get(path)
+        .then(result => {
+          this.todolist = result.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+
+  created() {
+    this.getTodoList();
+  }
 };
 </script>
