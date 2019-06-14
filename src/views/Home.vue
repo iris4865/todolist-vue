19cbdb21b8909a5f3193a9691eb08425aed737ab
munseong.kim <iris4865@gmail.com>
2019-06-14 16:14:12 +0900

    Update newTaskModal -> TaskForm

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,10 @@
-<template>
+﻿<template>
   <b-container>
     <h1 class="text-center">TodoList</h1>
     <b-button
-      v-b-modal.new-task-modal
+      v-b-modal.task-modal
       v-on:click="newTask()"
-      class="btn btn-success btn-block"
-    >New Task</b-button>
+      class="btn btn-success btn-block">New Task</b-button>
     <hr>
     <b-row>
       <table class="table table-hover">
@@ -17,26 +16,35 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(todo) in todolist" :key="todo.id" v-bind:class="{'table-danger': isExpire(todo.end_date, todo.complete)}" >
-            <td
-                v-bind:class="{'isComplete': todo.complete,
-                              'table-danger': isExpire(todo.end_date, todo.complete)}"
-            >{{ todo.title }}</td>
-
+          <tr v-for="(todo) in todolist"
+              :key="todo.id"
+              v-bind:class="{'table-danger': isExpire(todo.end_date, todo.complete)}">
+            <td v-bind:class="{
+                                'isComplete': todo.complete,
+                                'table-danger': isExpire(todo.end_date, todo.complete)
+                              }">{{ todo.title }}</td>
             <td style="width: 200px"
-                v-bind:class="{'isComplete': todo.complete,
-                              'table-danger': isExpire(todo.end_date, todo.complete)}"
-            >{{ todo.end_date }}</td>
-
+                v-bind:class="{
+                                'isComplete': todo.complete,
+                                'table-danger': isExpire(todo.end_date, todo.complete)}">
+              {{ todo.end_date }}
+            </td>
             <td style="width: 250px">
-              <b-button v-on:click="upTask(todo.id)" variant="info" style="margin: 1px">▲</b-button>
-              <b-button v-on:click="downTask(todo.id)" variant="info" style="margin: 1px">▼</b-button>
+              <b-button v-on:click="upTask(todo.id)"
+                        variant="info"
+                        style="margin: 1px">
+                ▲
+              </b-button>
+              <b-button v-on:click="downTask(todo.id)"
+                        variant="info"
+                        style="margin: 1px">
+                ▼
+              </b-button>
               <b-button
                 v-b-modal.update-task-modal
                 v-on:click="editTask(todo)"
                 variant="warning"
-                style="margin: 1px"
-              >Edit</b-button>
+                style="margin: 1px">Edit</b-button>
               <b-button v-on:click="deleteTask(todo.id)" variant="danger">Delete</b-button>
             </td>
           </tr>
@@ -44,47 +52,10 @@
       </table>
     </b-row>
 
-    <b-modal ref="newTaskModal" id="new-task-modal" title="New" hide-footer>
-      <b-form @submit.prevent="onAddTask" @reset.prevent="onResetNewTask" class="w-100">
-        <b-form-group id="form-title-new-group" label="Title:" label-for="form-title-new-input">
-          <b-form-input
-            id="form-title-new-input"
-            type="text"
-            v-model="task.title"
-            required
-            placeholder="Enter title"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          id="form-description-new-group"
-          label="Description:"
-          label-for="form-author-new-input"
-        >
-          <b-form-textarea
-            id="form-description-new-textarea"
-            v-model="task.description"
-            placeholder="Enter description"
-          ></b-form-textarea>
-        </b-form-group>
-        <b-form-group
-          id="form-end_date-new-group"
-          label="End Date:"
-          label-for="form-author-new-input"
-        >
-          <b-form-input
-            id="form-end_date-new-input"
-            type="date"
-            v-model="task.end_date"
-            placeholder="Enter End Date"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-checkbox id="form-complete-checkbox" v-model="task.complete">Complete Task</b-form-checkbox>
-
-        <b-button-group>
-          <b-button type="submit" variant="primary">OK</b-button>
-          <b-button type="reset" variant="danger">Cancel</b-button>
-        </b-button-group>
-      </b-form>
+    <b-modal ref="taskModal" id="task-modal" title="New" hide-footer>
+      <TaskForm v-bind:task="task"
+                @okTask="okTask"
+                @cancelTask="cancelTask"/>
     </b-modal>
 
     <b-modal ref="editTaskModal" id="update-task-modal" title="Update" hide-footer>
@@ -101,8 +72,7 @@
         <b-form-group
           id="form-description-edit-group"
           label="Description:"
-          label-for="form-author-edit-input"
-        >
+          label-for="form-author-edit-input">
           <b-form-textarea
             id="form-description-edit-textarea"
             v-model="task.description"
@@ -112,8 +82,7 @@
         <b-form-group
           id="form-end_date-edit-group"
           label="End Date:"
-          label-for="form-author-edit-input"
-        >
+          label-for="form-author-edit-input">
           <b-form-input
             id="form-end_date-edit-input"
             type="date"
@@ -122,7 +91,10 @@
             placeholder="Enter End Date"
           ></b-form-input>
         </b-form-group>
-        <b-form-checkbox id="form-complete-checkbox" v-model="task.complete">Complete Task</b-form-checkbox>
+        <b-form-checkbox id="form-complete-checkbox"
+                         v-model="task.complete">
+          Complete Task
+        </b-form-checkbox>
 
         <b-button-group>
           <b-button type="submit" variant="primary">Update</b-button>
@@ -134,20 +106,23 @@
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue';
 
 import axios from 'axios';
+import TaskForm from '@/components/TaskForm.vue';
 
 export default {
   name: 'home',
+  components: {
+    TaskForm,
+  },
+
   data() {
     return {
       new_task_title: '',
 
       task: {
         id: 0,
-        title: '',
+        title: '33',
         description: '',
         priority: 0,
         create_date: '',
@@ -161,7 +136,7 @@ export default {
 
   methods: {
     getTodoList() {
-      const path = 'api/task/';
+      const path = 'http://127.0.0.1:5000/api/task/';
 
       axios
         .get(path)
@@ -175,7 +150,7 @@ export default {
     },
 
     isExpire(date, isComplete) {
-      if (isComplete) {
+      if (isComplete || date === '') {
         return false;
       }
 
@@ -191,7 +166,7 @@ export default {
 
     newTask() {
       this.task = {
-        id: 0,
+        id: -1,
         title: '',
         description: '',
         priority: 0,
@@ -201,20 +176,26 @@ export default {
       };
     },
 
-    onAddTask() {
-      this.$refs.newTaskModal.hide();
+    okTask() {
+      this.$refs.taskModal.hide();
+
       const payload = {
         title: this.task.title,
         description: this.task.description,
         end_date: this.task.end_date,
         complete: this.task.complete,
       };
-      this.addTask(payload);
+
+      if (this.task.id === -1) { // new
+        this.addTask(payload);
+      } else {
+        this.updateTask(this.task.id, payload);
+      }
     },
 
     upTask(id) {
-      for (let index = 1; index < this.todolist.length; index++) {
-        if (id == this.todolist[index].id) {
+      for (let index = 1; index < this.todolist.length; index += 1) {
+        if (id === this.todolist[index].id) {
           const now = this.todolist[index];
           const prev = this.todolist[index - 1];
           this.updateTask(now.id, { priority: prev.priority });
@@ -225,8 +206,8 @@ export default {
     },
 
     downTask(id) {
-      for (let index = 0; index < this.todolist.length - 1; index++) {
-        if (id == this.todolist[index].id) {
+      for (let index = 0; index < this.todolist.length - 1; index += 1) {
+        if (id === this.todolist[index].id) {
           const now = this.todolist[index];
           const next = this.todolist[index + 1];
           this.updateTask(now.id, { priority: next.priority });
@@ -258,7 +239,7 @@ export default {
     },
 
     addTask(payload) {
-      const path = 'api/task/';
+      const path = 'http://127.0.0.1:5000/api/task/';
       axios
         .post(path, payload)
         .then((res) => {
@@ -276,7 +257,7 @@ export default {
     },
 
     updateTask(taskID, payload) {
-      const path = `api/task/${taskID}`;
+      const path = `http://127.0.0.1:5000/api/task/${taskID}`;
       axios
         .put(path, payload)
         .then((res) => {
@@ -292,7 +273,7 @@ export default {
     },
 
     deleteTask(taskID) {
-      const path = `api/task/${taskID}`;
+      const path = `http://127.0.0.1:5000/api/task/${taskID}`;
       axios
         .delete(path)
         .then((res) => {
@@ -306,8 +287,8 @@ export default {
         });
     },
 
-    onResetNewTask() {
-      this.$refs.newTaskModal.hide();
+    cancelTask() {
+      this.$refs.taskModal.hide();
       this.getTodoList();
     },
 
